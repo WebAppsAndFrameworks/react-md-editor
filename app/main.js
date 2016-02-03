70864c0dd794bc4c9a50f5111a9d26d5c999a05e
John-David Dalton <john.david.dalton@gmail.com>
2016-02-03 14:16:16 -0800

    Replace tabs with spaces.

--- app/main.js
+++ app/main.js
@@ -1,43 +1,42 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Editor = require('react-medium-editor');
+
 require('medium-editor/dist/css/medium-editor.css');
 require('medium-editor/dist/css/themes/default.css');
 
 var Main = React.createClass({
-	getInitialState: function() {
-		return {
-			text: 'Fusce dapibus, tellus ac cursus commodo'
-		};
-	},
+  getInitialState() {
+    return {
+      'text': 'Fusce dapibus, tellus ac cursus commodo'
+    };
+  },
 
-	handleChange: function(text, medium) {
-		this.setState({
-			text: text
-		});
+  handleChange(text, medium) {
+    this.setState({
+      'text': text
+    });
+  },
 
-		console.log('medium', medium);
-	},
-
-	render: function() {
-		return (
-			<div className="container">
-				<div className="row">
-					<div className="col-xs-12">
-						<h1>React Markdown Editor</h1>
-					</div>
-				</div>
-				<div className="row">
-					<div className="col-xs-6">
-						{this.state.text}
-					</div>
-					<div className="col-xs-6">
-						<Editor text={ this.state.text } onChange={ this.handleChange } />
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12">
+            <h1>React Markdown Editor</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-6">
+            {this.state.text}
+          </div>
+          <div className="col-xs-6">
+            <Editor text={ this.state.text } onChange={ this.handleChange } />
+          </div>
+        </div>
+      </div>
+    );
+  }
 });
 
 ReactDOM.render(<Main />, document.getElementById('app'));
