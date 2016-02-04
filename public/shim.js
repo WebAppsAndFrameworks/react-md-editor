3e77dd681c5d702ab3266126f5fed696cfa5b21a
Zack Hall <zackhall@outlook.com>
2016-02-04 09:32:13 -0800

    Thanks, @jdalton. Update execCommand shim.

--- public/shim.js
+++ public/shim.js
@@ -1,21 +1,52 @@
-document.execCommand = (function(func) {
-  return function(cmd, ui, arg) {
-    var anchorNode = window.getSelection().anchorNode;
-    var element = anchorNode ? anchorNode.parentNode : null;
-    if (element && element.isContentEditable) {
-      var isBuggy = true;
-      var capture = function() { isBuggy = false; };
-      
-      element.addEventListener('input', capture, true);
-
-      var result = func.call(document, cmd, ui, arg);
-      element.removeEventListener('input', capture, true);
-      
-      if (isBuggy) {
-        var event = document.createEvent('Event');
-        event.initEvent('input', true, false);
-        element.dispatchEvent(event);
-      }
+document.execCommand = (function(_execCommand) {
+  var isBuggy = true;
+  
+  function complete(element, result) {
+    if (result) {
+      var event = document.createEvent('Event');
+      event.initEvent('input', true, false);
+      element.dispatchEvent(event);
     }
-  };
+    return result;
+  }
+  
+  function getEditable(element) {
+    while (element &&
+        !(element.nodeType == 1 && element.hasAttribute('contenteditable'))) {
+      element = element.parentNode;
+    }
+    return element;
+  }
+  
+  function restore() {
+    isBuggy = false;
+    document.execCommand = _execCommand;
+  }
+  
+  function exec(cmd, ui, arg) {
+    return _execCommand.call(document, cmd, ui, arg);
+  }
+  
+  function shimExec(element, cmd, ui, arg) {
+    return complete(element, exec(cmd, ui, arg));
+  }
+  
+  function shim(element, cmd, ui, arg) {
+    element.addEventListener('input', restore, true);
+    var result = exec(cmd, ui, arg);
+    element.removeEventListener('input', restore, true);
+    if (isBuggy) {
+      shim = shimExec;
+      return complete(element, result);
+    }
+  }
+
+  function wrapper(cmd, ui, arg) {
+    var element = getEditable(getSelection().anchorNode);
+    return (element && element.isContentEditable)
+      ? shim(element, cmd, ui, arg)
+      : exec(cmd, ui, arg);
+  }
+  
+  return wrapper;
 }(document.execCommand));
