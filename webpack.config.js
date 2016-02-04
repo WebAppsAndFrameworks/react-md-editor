73903dfd593d164468076842576e65ef8d8e6cd0
John-David Dalton <john.david.dalton@gmail.com>
2016-02-04 10:51:17 -0800

    Add production build scripts to package.json.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
-module.exports = {
-  'entry': './app/main.js',
-  'output': {
-    'filename': './public/dist/index.js'
-  },
+'use strict';
+
+var webpack = require('webpack');
+
+var env = process.env.NODE_ENV;
+
+var config = {
   'devServer': {
     'host': '0.0.0.0',
     'inline': true,
@@ -11,14 +13,32 @@ module.exports = {
   'module': {
     'loaders': [
       {
+        'test': /\.jsx?$/,
         'exclude': /node_modules/,
         'loader': 'babel',
-        'query': {
-          'presets': ['react', 'es2015']
-        },
-        'test': /\.jsx?$/
+        'query': { 'presets': ['react', 'es2015'] }
       },
-      { test: /\.css$/, loader: "style-loader!css-loader" }
+      {
+        'test': /\.css$/,
+        'loader': 'style-loader!css-loader'
+      }
     ]
-  }
+  },
+  'plugins': []
 };
+
+if (env == 'production') {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      'compressor': {
+        'pure_getters': true,
+        'screw_ie8': true,
+        'unsafe': true,
+        'unsafe_comps': true,
+        'warnings': false
+      }
+    })
+  );
+}
+
+module.exports = config;
